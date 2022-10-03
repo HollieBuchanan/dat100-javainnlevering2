2e0775dcc9eef4c4584cecef1e70543b7f148153
Hollie <Hollie@MSI>
2022-10-04 00:15:47 +0200

    Oblig 2 innlevering

--- src/no/hvl/dat100/lab5/tabeller/Tabeller.java
+++ src/no/hvl/dat100/lab5/tabeller/Tabeller.java
@@ -1,64 +1,120 @@
 package no.hvl.dat100.lab5.tabeller;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Tabeller {
 
 	// a)
 	public static void skrivUt(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
+		System.out.print("[ ");
+		for (int n : tabell) {
+			System.out.print(n + " ");
+		}
+		
+		System.out.print("]");
 
 	}
 
 	// b)
 	public static String tilStreng(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
+    	System.out.print("[ ");
+    	for (int tall : tabell) {
+    		System.out.print(tall + ",");
+    	}
+    	System.out.print(" ]");
+    	String print= (" ");
+		return print;
 		
 	}
 
 	// c)
 	public static int summer(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("summer ikke implementert");
+		int sum = 0;
+		for (int n : tabell) {
+			sum = sum + n;
+		}
+    	return sum;
 	}
 
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
-
+		Scanner inn = new Scanner(System.in);
+		String read = inn.nextLine();
+		int readNumber = Integer.parseInt(read);
+		boolean resault = false;
+		for (int n : tabell) {
+			if (readNumber == n) {
+				resault = true;
+			}
+		}
+		System.out.println(resault);
+		return resault;
 		// TODO
-		throw new UnsupportedOperationException("finnesTall ikke implementert");
 	}
-
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
-
-		// TODO
-		throw new UnsupportedOperationException("posisjonTall ikke implementert");
-
-	}
+		Scanner inn1 = new Scanner(System.in);
+		String read1 = inn1.nextLine();
+		int readNumber1 = Integer.parseInt(read1);
+			
+			 if (tabell == null) {
+		            return -1;
+		        }
+			int length = tabell.length;
+			int x = 0;
+			boolean notThere = false;
+			while (x < length) {
+				if (tabell[x] == tall) {
+					System.out.print("Posisjonen av " + tabell[x] + " er " + (x+1));
+					return x;	
+				}
+				if (x != tall) {
+					notThere = true;
+				}
+				x++;
+			}
+			if (notThere = true) {
+				System.out.print(-1);
+			}
+			return -1;
+			}
 
 	// f)
 	public static int[] reverser(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("reverser ikke implementert");
-		
+		int x = tabell.length;
+		int[] substitute = new int [x];
+		int z = x;
+		for (int i = 0; i < x; i++) {
+			substitute[z-1] = tabell[i];
+			z = z - 1;
+		}
+		System.out.println("Tabellen i revers er:");
+		 for (int y = 0; y < x; y++) {
+	            System.out.print(substitute[y]);
+		 }
+		 return substitute;
 	}
 
 	// g)
 	public static boolean erSortert(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("erSortert ikke implementert");
+		boolean done = false;
+		Arrays.sort(tabell);
+		System.out.println("Stigende rekkefølge på arrayen er: ");
+		for (int x = 0; x < tabell.length; x++ ) {
+			System.out.print(tabell[x] + ",");
+		}
+		return done;
 	}
 
 	// h)
 	public static int[] settSammen(int[] tabell1, int[] tabell2) {
-
-		// TODO
-		throw new UnsupportedOperationException("settSammen ikke implementert");
+			int first = tabell1.length;
+			int second = tabell2.length;
+			int sammen[] = new int [first+second];
+			System.arraycopy(tabell1, 0, sammen, 0, first);
+			System.arraycopy(tabell2, 0, sammen, first, second);
+			System.out.println(Arrays.toString(sammen));
+			return sammen;
 	}
 }
--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -4,32 +4,85 @@ public class Matriser {
 
 	// a)
 	public static void skrivUt(int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
+		for (int x = 0; x < matrise.length; x++) {
+			for (int y = 0; y < matrise [x].length; y++) {
+				System.out.print(matrise[x][y] + " ");
+			}
+		}
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
-
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
-		
+		System.out.print("{");
+		for (int x = 0; x < matrise.length-1; x++) {
+			System.out.print("{ ");
+			for (int y = 0; y < matrise [x].length; y++) {
+				String result = (matrise[x][y] + " ");
+				System.out.print(result);
+			}
+			System.out.print("}" + "\n");
+		}
+		for (int x = matrise.length-1; x < matrise.length; x++) {
+			System.out.print("{ ");
+			for (int y = 0; y < matrise [x].length; y++) {
+				String result = (matrise[x][y] + " ");
+				System.out.print(result);
+			}
+			System.out.print("}}");
+		}
+		return null;	
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("skaler ikke implementert");
+		System.out.print("{");
+		int [][] multiplisert = {{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0}};
+		for (int x = 0; x < matrise.length-1; x++) {
+			System.out.print("{ ");
+			for (int y = 0; y < matrise [x].length; y++) {
+				multiplisert [x][y] = matrise[x][y]*tall;
+				System.out.print(multiplisert [x][y] + " ");
+			}
+			System.out.print("}" + "\n");
+		}
+		for (int x = matrise.length-1; x < matrise.length; x++) {
+			System.out.print("{ ");
+			for (int y = 0; y < matrise [x].length; y++) {
+				multiplisert [x][y] = matrise[x][y]*tall;
+				System.out.print(multiplisert [x][y] + " ");
+			}
+			System.out.print("}}");
+		}
+		return multiplisert;
 	
 	}
 
 	// d)
-	public static boolean erLik(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("erLik ikke implementert");
+	public static boolean erLik(int[][] mat1, int[][] mat2) {
+		boolean same = true;
+		int rekke1 = mat1.length;
+		int rekke2 = mat2.length;
+		int rad1 = mat1[0].length;
+		int rad2 = mat2[0].length;
+		if(rekke1 != rekke2 || rad1 != rad2) {
+			same = false;
+		}
+		else {
+		for (int x = 0; x < mat1.length; x++) {
+				for (int y = 0; y < mat1[x].length; y++) {
+					if (mat1[x][y] != mat2[x][y]) {
+						same = false;
+					}
+				}
+			}
+		}
+		if (same = false) {
+			System.out.println("Matrisene er ikke lik.");
+		}
+		else {
+			System.out.println("Matrisene er lik.");
+		}
+		return same;
 	}
 	
 	// e)
